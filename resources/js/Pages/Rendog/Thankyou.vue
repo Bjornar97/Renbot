50add3cfe2cfee95cc1066e0c29d5fcb95a376a4
Bjørnar Hetlesæther <hetlest@hotmail.com>
2023-02-13 22:29:20 +0100

    Fixed rendog logo on thank you page

--- resources/js/Pages/Rendog/Thankyou.vue
+++ resources/js/Pages/Rendog/Thankyou.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ModeratorLayout from "@/Layouts/ModeratorLayout.vue";
+import RendogLogo from "../../../images/rendog-logo.png";
 
 defineOptions({
     layout: ModeratorLayout,
@@ -8,11 +9,7 @@ defineOptions({
 
 <template>
     <div class="page">
-        <img
-            class="mx-auto mb-4 mt-12"
-            src="/images/rendog-logo.png"
-            alt="Rendog logo"
-        />
+        <img class="mx-auto mb-4 mt-12" :src="RendogLogo" alt="Rendog logo" />
         <h1 class="text-md-h1 mb-8 mx-auto text-center">Thank you Rendog</h1>
 
         <p class="info text-center mx-auto">
