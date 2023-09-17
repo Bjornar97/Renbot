2c299544b9553913a52a81c1e30f853786bed167
Bjørnar Hetlesæther <hetlest@hotmail.com>
2023-09-17 21:18:27 +0200

    Updated text on rendog thankyou page

--- resources/js/Pages/Rendog/Thankyou.vue
+++ resources/js/Pages/Rendog/Thankyou.vue
@@ -10,11 +10,20 @@ defineOptions({
 <template>
     <div class="page">
         <img class="mx-auto mb-4 mt-12" :src="RendogLogo" alt="Rendog logo" />
-        <h1 class="text-md-h1 mb-8 mx-auto text-center">Thank you Rendog</h1>
+        <h1 class="text-md-h1 mb-8 mx-auto text-center">Welcome Rendog!</h1>
+
+        <p class="info text-center mx-auto mb-6">
+            This is the portal that all moderators can use. You can create and
+            edit commands, change the rules and change some settings in the bot.
+            There is a menu to the left on desktop, and top left and bottom on
+            mobile.
+        </p>
 
         <p class="info text-center mx-auto">
-            That is all you have to do. You can now leave this site if you want,
-            or you can see commands and chat rules. There is a menu to the left.
+            For a list of normal commands, you can click "Regular commands".
+            "Punishable commands" are commands that when you use them, you have
+            to specify a username. That username will get timed out and sent a
+            message that they did something wrong.
         </p>
     </div>
 </template>
@@ -28,5 +37,6 @@ defineOptions({
 
 .info {
     max-width: 70ch;
+    line-height: 1.75;
 }
 </style>
