0eeb9c51fd8ff1f9de380ef27ea9dc246ac5ee33
Bjornar97 <hetlest@hotmail.com>
2024-09-08 14:00:07 +0000

    Use API to send single chat message

--- app/Http/Controllers/Api/PunishController.php
+++ app/Http/Controllers/Api/PunishController.php
@@ -5,13 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Jobs\SingleChatMessageJob;
 use App\Models\Command;
-use App\Services\BotService;
 use App\Services\PunishService;
 use App\Services\TwitchService;
-use GhostZero\Tmi\Events\Irc\WelcomeEvent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Log;
 
 class PunishController extends Controller
 {
