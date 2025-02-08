f314220babcad1dcf9627f2ea5f1ecc56589a61c
Bjørnar Hetlesæther <bjornar.h+github@hey.com>
2025-02-08 20:26:55 +0100

    Add cooldown when sending command to chat. (#68)

--- resources/js/Components/Shared/Messages.vue
+++ resources/js/Components/Shared/Messages.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { usePage } from "@inertiajs/vue3";
+import { mdiAlert } from "@mdi/js";
 import { computed, ref, watch } from "vue";
 
 const flash = ref<any>(usePage()?.props?.flash);
@@ -42,6 +43,22 @@ const showSuccess = computed({
     },
 });
 
+const showWarning = computed({
+    get: (): boolean => {
+        return isMessageShowable(flash.value?.warning);
+    },
+    set: (v: boolean) => {
+        showSnacks.value = v;
+        // We remove the value after 500ms, so it doesnt get removed until the animation is done.
+        // We remove manually because Inertia does not at the time of writing.
+        if (!v) {
+            setTimeout(() => {
+                flash.value.warning = undefined;
+            }, 500);
+        }
+    },
+});
+
 watch(
     () => (usePage().props as any).flash,
     (newValue) => {
@@ -59,7 +76,7 @@ watch(
         <VSnackbar
             :timeout="5000"
             color="success"
-            class="success-snackbar"
+            class="bottom-right-snackbar"
             v-model="showSuccess"
             location="bottom right"
             transition="slide-x-reverse-transition"
@@ -89,18 +106,33 @@ watch(
                 <li v-for="info in flash?.info">{{ info }}</li>
             </ul>
         </VAlert>
-        <VAlert
-            class="mb-2"
-            type="warning"
-            v-if="flash?.warning && flash?.warning.length > 0"
+        <VSnackbar
+            :timeout="5000"
+            color="warning"
+            class="bottom-right-snackbar"
+            v-model="showWarning"
+            location="bottom right"
+            transition="slide-x-reverse-transition"
         >
-            <p v-if="typeof flash?.warning === 'string'">
-                {{ flash?.warning }}
-            </p>
-            <ul v-else>
-                <li v-for="warning in flash?.warning">{{ warning }}</li>
-            </ul>
-        </VAlert>
+            <div class="d-flex align-center ga-2">
+                <VIcon :icon="mdiAlert"></VIcon>
+                <p v-if="typeof flash?.warning === 'string'" class="mb-0">
+                    {{ flash?.warning }}
+                </p>
+                <ul v-else>
+                    <li v-for="warning in flash?.warning">{{ warning }}</li>
+                </ul>
+            </div>
+
+            <template v-slot:actions>
+                <VBtn
+                    color="white"
+                    variant="text"
+                    @click="showWarning = false"
+                    >{{ "Close" }}</VBtn
+                >
+            </template>
+        </VSnackbar>
         <VAlert
             class="mb-2"
             type="error"
@@ -116,7 +148,7 @@ watch(
 </template>
 
 <style>
-.success-snackbar .v-overlay__content {
+.bottom-right-snackbar .v-overlay__content {
     position: fixed;
     bottom: 4rem !important;
 }
