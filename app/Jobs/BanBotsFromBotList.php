ce75a48ec4b5f22d12a15506b3e83694f7d97af1
Bjornar97 <bjornar.h+github@hey.com>
2025-07-31 12:27:50 +0000

    Fix typing fo bots online and remove hardcoded username.

--- app/Jobs/BanBotsFromBotList.php
+++ app/Jobs/BanBotsFromBotList.php
@@ -40,13 +40,12 @@ class BanBotsFromBotList implements ShouldQueue
             // return;
         }
 
-        /** @var array<string, array<int, array<string|int>>> $response */
+        /** @var array{bots: list<array{0: string, 1: int, 2: int}>} $response */
         $response = Http::get('https://api.twitchinsights.net/v1/bots/online')->json();
 
-        $bots = collect($response['bots'] ?? []);
-        $bots = $bots->pluck(0);
+        $bots = collect($response['bots'])->map(static fn($item) => $item[0]);
 
-        $renbotUser = User::query()->where('username', 'RenTheBot')->first();
+        $renbotUser = User::query()->where('username', config("services.twitch.username"))->first();
 
         $twitch = new Twitch;
         $twitch->setToken($renbotUser->twitch_access_token);
